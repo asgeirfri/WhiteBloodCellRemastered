849ec5913e700b40fbeaac8b2400fd02fa31531e
Á Frímanns <asgeir@bokun.is>
2020-05-29 15:20:12 +0000

    First commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float startSpeed = 2;
+    private Rigidbody2D rb2d;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 fingerPosition = Input.mousePosition;
+
+        Debug.Log((fingerPosition - rb2d.position).normalized.ToString());
+
+        Vector2 direction = (fingerPosition - rb2d.position).normalized;
+
+        rb2d.velocity = (direction * startSpeed);
+    }
+}
