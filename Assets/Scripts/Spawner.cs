764e2e3add3e03bc40393db535ce21476305e33f
Á Frímanns <asgeir@bokun.is>
2020-05-29 22:13:26 +0000

    Spawner spawns

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public float minWait;
+    public float maxWait;
+
+    public GameObject[] enemyTypes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello BITCH");
+        StartCoroutine(SpawnInSeconds(Random.Range(minWait, maxWait)));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator SpawnInSeconds(float seconds)
+    {
+        Debug.Log("Hello");
+        yield return new WaitForSeconds(seconds);
+        Spawn();
+        StartCoroutine(SpawnInSeconds(Random.Range(minWait, maxWait)));
+    }
+
+    void Spawn()
+    {
+        Instantiate(enemyTypes[Random.Range(0, enemyTypes.Length - 1)], transform.position, Quaternion.identity);
+    }
+}
